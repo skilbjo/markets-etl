3a184bc5be5c60e2bda07eedcbe462bde5fe6b5f
John Skilbeck <john.skilbeck@gmail.com>
2017-04-12 23:20:01 -0700

    switched test call for live call

--- src/jobs/equities.clj
+++ src/jobs/equities.clj
@@ -50,17 +50,16 @@
                                                                 "date    = ?        ")
                                                                dataset ticker date]
                                                               m)) col))]
-    (->> f/fixture-multi                    ; Testing
-         flatten
-         (map clean-dataset)
-         (map database-it)
-         flatten
-    ;(->> (map get-quandl-data datasets)    ; Live call
+    ;(->> f/fixture-multi                  ; Testing
          ;flatten
          ;(map clean-dataset)
          ;(map database-it)
          ;flatten
-         ;util/printit
+    (->> (map get-quandl-data datasets)    ; Live call
+         flatten
+         (map clean-dataset)
+         (map database-it)
+         flatten
          (map-update-or-insert! :dw.equities)
          util/printit
          )))
