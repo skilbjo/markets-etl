82c73a4e8e59c4f7ebe2839eafb43ed96fae3ef3
John Skilbeck <john.skilbeck@gmail.com>
2018-01-30 11:38:21 -0800

    Clojure 1.9 :tada:

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject markets-etl "0.1.0"
   :uberjar-name "app.jar"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.7.0"]
                  [clj-time "0.14.2"]
                  [environ "1.1.0"]
