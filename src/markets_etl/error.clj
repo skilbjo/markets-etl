091ed4254e8acc92cbf494690981499069ecf6ba
John Skilbeck <john.skilbeck@gmail.com>
2018-02-13 18:24:18 -0800

    Better default error handler

--- src/markets_etl/error.clj
+++ src/markets_etl/error.clj
@@ -1,9 +1,13 @@
 (ns markets-etl.error
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log])
+  (:import  [clojure.lang IExceptionInfo]))
 
 (defn set-default-error-handler []
   (Thread/setDefaultUncaughtExceptionHandler
    (reify Thread$UncaughtExceptionHandler
      (uncaughtException [_ thread e]
        (log/error e
-                  (str "Uncaught exception on " (.getName thread)))))))
+                  (str "Uncaught exception on " (.getName thread)))
+       (when (instance? IExceptionInfo e)
+         (log/error (ex-data e)))
+       (System/exit 1)))))
