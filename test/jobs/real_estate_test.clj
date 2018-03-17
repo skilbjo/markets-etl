f06f17795ebecab363817a2ce3682dffce7356cd
John Skilbeck <john.skilbeck@gmail.com>
2018-03-16 20:08:44 -0700

    Add dw_created_at field

--- test/jobs/real_estate_test.clj
+++ test/jobs/real_estate_test.clj
@@ -3,18 +3,30 @@
             [clojure.java.jdbc :as jdbc]
             [clojure.test :refer :all]
             [environ.core :refer [env]]
+            [markets-etl.util :as util]
             [jobs.real-estate :refer :all :rename {-main _}]
             [fixtures.real-estate :as f]
             [fixtures.fixtures :refer [*cxn*] :as fix]))
 
 (use-fixtures :each (fix/with-database))
 
+(defn contains-every-key? [m & ks]
+  (every? #(contains? m %) ks))
+
 (deftest integration-test
   (->> f/source
        (execute! *cxn*))
 
   (testing "real_estate integration test"
-    (is (= f/result
-           (->> "select * from dw.real_estate"
-                (jdbc/query *cxn*)
-                flatten)))))
+    (let [actual  (->> "select * from dw.real_estate"
+                       (jdbc/query *cxn*)
+                       flatten)]
+      (is (= f/result
+             (->> actual
+                  (map #(dissoc %
+                                :dw_created_at)))))
+      (is (every? true?
+                  (->> actual
+                       (map #(contains? %
+                                        :dw_created_at)))))
+      (is (not (empty? actual))))))
